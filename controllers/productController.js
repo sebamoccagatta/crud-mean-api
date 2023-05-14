9c8a56e20974756f4238eddf7922572c5f95c7a2
seba_moccagatta <sebamoccagatta1@gmail.com>
2023-05-13 23:22:45 -0300

    add editProduct endpoint

--- controllers/productController.js
+++ controllers/productController.js
@@ -20,12 +20,35 @@ exports.createProduct = async (req, res) => {
 }
 
 exports.getProducts = async (req, res) => {
-
     try {
 
         const products =  await Product.find();
 
         res.json(products);
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error');
+    }
+}
+
+exports.editProduct = async (req, res) => {
+
+    try {
+        const {name, category, locate, price} = req.body;
+        let product =  await Product.findById(req.params.id);
+
+        if(!product)
+            res.status(404).json({message: 'No existe el Producto'});
+        
+        product.name = name;
+        product.category = category;
+        product.locate = locate;
+        product.price = price;
+
+        product = await Product.findOneAndUpdate({ _id: req.params.id}, product, {new: true});
+
+        res.json(product);
         
     } catch (error) {
         console.log(error);
--- routes/product.js
+++ routes/product.js
@@ -6,5 +6,5 @@ const productController = require('../controllers/productController');
 // api/product
 router.get('/', productController.getProducts);
 router.post('/create-product', productController.createProduct);
-
+router.put('/edit-product/:id', productController.editProduct);
 module.exports = router;
